c138c2095731721919fa8a91b5fc7baf462fe70c
ramalavalapati <43298004+ramalavalapati@users.noreply.github.com>
2021-01-29 05:45:12 -0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,1 +1,12 @@
-Hello Java
+package Pkg1;
+
+public class hello_Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+
+	}
+
+}
